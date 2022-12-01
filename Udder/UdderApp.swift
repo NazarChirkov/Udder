2da90d303a09e59a4c04349dcccdcdc99de92627
Nazar <nazarchirkov@icloud.com>
2022-12-01 20:42:37 +0000

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Nazar Chirkov on 1.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
